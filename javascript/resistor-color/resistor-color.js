85bc7be2a9b62fcb245d7a111e80c43867d30a0b
Kris <bluegiantsoftware@gmail.com>
2021-04-16 11:36:20 -0400

    completed resistor-color javascript track

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (inputColor) => {
+  for(let [index, color] of COLORS.entries()) {
+    if(color == inputColor) {
+      return index;
+    }
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = ['black','brown','red','orange', 'yellow','green','blue','violet','grey','white'];
