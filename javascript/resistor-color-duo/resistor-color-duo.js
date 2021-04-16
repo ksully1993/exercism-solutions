cd6487d25e62fe0f1f99ce70d11cae74735b0d8f
Kris <bluegiantsoftware@gmail.com>
2021-04-16 13:17:44 -0400

    completed resistor-color-duo javascript track

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decodedValue = (colorArr) => {
+  const colorCode = (inputColor) => {
+
+    const colors = ['black','brown','red','orange', 'yellow','green','blue','violet','grey','white'];
+
+    for(let [index, color] of colors.entries()) {
+      if(color == inputColor) {
+        return index;
+      }
+    }
+  }
+  
+  const value = () => {
+    return ((10 * colorCode(colorArr[0])) + colorCode(colorArr[1]));
+  }
+  
+  return value();
 };
