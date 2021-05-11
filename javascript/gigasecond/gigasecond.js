f08b98f831a68dd3f3809bc8518caced3a5c8355
Kris <bluegiantsoftware@gmail.com>
2021-05-11 04:03:48 -0400

    completed gigasecond javascript track

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,8 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  // gigasecond = 1,000,000,000
+  // milliseconds = gigasecond * 1000
+  const msInGigasecond = 1000000000000;
+  const afterGigasecond = new Date();
+  afterGigasecond.setTime(date.getTime() + msInGigasecond);
+  return afterGigasecond;
 };
