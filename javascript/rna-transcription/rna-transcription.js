3163511d27afd9867d7d6579b974408a51f6074c
Kris <bluegiantsoftware@gmail.com>
2021-05-14 02:19:11 -0400

    completed rna-transcription javascript track

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRna = (input) => {
+  const dna = input.split('');
+  const rna = [];
+  dna.forEach(nucleotide => {
+    if(nucleotide === 'G'){
+      rna.push('C');
+    }
+    else if(nucleotide === 'C'){
+      rna.push('G');
+    }
+    else if(nucleotide === 'T'){
+      rna.push('A');
+    }
+    else if(nucleotide === 'A'){
+      rna.push('U');
+    }
+  });
+  return rna.join('');
+};
